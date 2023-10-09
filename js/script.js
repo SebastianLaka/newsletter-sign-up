4e6e6a2728cea53fa54882c5cf9fc48cbc6c7e78
Sebastian <sebastianlakla@gmail.com>
2023-10-09 20:36:14 +0200

    Add input variable and change ctaButton variable

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const ctaButton = document.querySelector(".verification-area__input")
-
+const input = document.querySelector(".verification-area__input")
+const ctaButton = document.querySelector('.verification-area__cta')
 ctaButton.addEventListener('click', ()=>{
     const errorInfo = document.querySelector('.label-area__requierd')
     console.log(errorInfo);
