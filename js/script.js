1ec6b9d435441c1021d25a7de1759083848b2d0d
Sebastian <sebastianlakla@gmail.com>
2023-10-09 20:27:35 +0200

    Get new variable

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@
 const ctaButton = document.querySelector(".verification-area__input")
 
 ctaButton.addEventListener('click', ()=>{
-    
+    const errorInfo = document.querySelector('.label-area__requierd')
+    console.log(errorInfo);
 })
