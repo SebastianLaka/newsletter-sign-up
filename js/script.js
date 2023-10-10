cdc7f94fa11ee2292be9e04c93c22268327394b3
Sebastian <sebastianlakla@gmail.com>
2023-10-10 20:34:23 +0200

    create function for error statement

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@
 const input = document.querySelector(".verification-area__input")
 const ctaButton = document.querySelector('.verification-area__cta')
 ctaButton.addEventListener('click', ()=>{
+    const showError = function(){
     if(input.value === ''){
         const errorInfo = document.querySelector('.label-area__requierd')
         errorInfo.style.visibility = 'visible';
     } 
+}
+showError();
 })
