ff0e18d273f1117b692f1b318b7e9df3ad909f1a
Sebastian <sebastianlakla@gmail.com>
2023-10-11 19:06:33 +0200

    retrieved popup after click and email. Add regex for email validation

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 
 const input = document.querySelector(".verification-area__input")
 const ctaButton = document.querySelector('.verification-area__cta')
+const newsletterSuccesPopup = document.querySelector('.newsletter-succes')
+const emailInContent = document.querySelector('.newsletter-about__info--email')
+console.log(newsletterSuccesPopup, emailInContent);
 ctaButton.addEventListener('click', ()=>{
-    const showError = function(){
+    
+    
+    let regex = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/;
+    const validateEmail = function(){
     if(input.value === ''){
         const errorInfo = document.querySelector('.label-area__requierd')
         errorInfo.style.visibility = 'visible';
     } 
 }
-showError();
+validateEmail();
 })
