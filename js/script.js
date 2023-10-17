d6bcf6306b2a0045414597b8207d0d6afe9a9950
Sebastian <sebastianlakla@gmail.com>
2023-10-17 21:13:25 +0200

    Create function for cta after send email

--- js/script.js
+++ js/script.js
@@ -6,17 +6,27 @@ const emailInContent = document.querySelector('.newsletter-about__info--email')
 const newsletterSuccesPopup = document.querySelector('.newsletter-succes')
 const newsletter = document.querySelector('.newsletter-body')
 const errorInfo = document.querySelector('.label-area__requierd')
+const dissMissCTA = document.querySelector('.newsletter-succes-body__cta')
+
 ctaButton.addEventListener('click', ()=>{
     let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]')
     const validateEmail = function(){
      if(input.value === 'email@company.com'){
             newsletterSuccesPopup.style.display = 'flex';
             errorInfo.style.visibility = 'hidden';
-            newsletter.style.display = 'none';
+            newsletter.style.display = 'none';        
     }
     else if(input.value === ''){
         errorInfo.style.visibility = 'visible'; 
-    } 
+    }
 }
+    const backToMain = function(){
+        if(dissMissCTA){
+            newsletterSuccesPopup.style.display = 'none';
+            newsletter.style.display ='grid';
+        }
+    }
+dissMissCTA.addEventListener('click', backToMain)
 validateEmail();
+
 })
