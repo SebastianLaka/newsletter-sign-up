e73af021f271570814260eee69fa7422a5364e8e
Sebastian <sebastianlakla@gmail.com>
2023-10-23 21:45:48 +0200

    add prevent default at e argument

--- js/script.js
+++ js/script.js
@@ -1,32 +1,39 @@
-'use strict';
+"use strict";
 
-const input = document.querySelector(".verification-area__input")
-const ctaButton = document.querySelector('.verification-area__cta')
-const emailInContent = document.querySelector('.newsletter-about__info--email')
-const newsletterSuccesPopup = document.querySelector('.newsletter-succes')
-const newsletter = document.querySelector('.newsletter-body')
-const errorInfo = document.querySelector('.label-area__requierd')
-const dissMissCTA = document.querySelector('.newsletter-succes-body__cta')
+const input = document.querySelector(".verification-area__input");
+const ctaButton = document.querySelector(".verification-area__cta");
+const emailInContent = document.querySelector(".newsletter-about__info--email");
+const newsletterSuccesPopup = document.querySelector(".newsletter-succes");
+const newsletter = document.querySelector(".newsletter-body");
+const errorInfo = document.querySelector(".label-area__requierd");
+const dissMissCTA = document.querySelector(".newsletter-succes-body__cta");
+const emailForValidation = document.getElementById("email");
 
-ctaButton.addEventListener('click', ()=>{
-    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]')
-    const validateEmail = function(){
-     if(input.value === 'email@company.com'){
-            newsletterSuccesPopup.style.display = 'flex';
-            errorInfo.style.visibility = 'hidden';
-            newsletter.style.display = 'none';        
+ctaButton.addEventListener("click", e => {
+  const validateEmail = function () {
+    e.preventDefault();
+    const isValid = function () {
+        let regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+         const active = regex.test();
+         return active
+      };
+      isValid();
+    if (input.value === isValid()) {
+      newsletterSuccesPopup.style.display = "flex";
+      errorInfo.style.visibility = "hidden";
+      newsletter.style.display = "none";
+      emailInContent.innetText = emailForValidation;
+    } else if (input.value === "") {
+      errorInfo.style.visibility = "visible";
     }
-    else if(input.value === ''){
-        errorInfo.style.visibility = 'visible'; 
-    }
-}
-    const backToMain = function(){
-        if(dissMissCTA){
-            newsletterSuccesPopup.style.display = 'none';
-            newsletter.style.display ='grid';
-        }
-    }
-dissMissCTA.addEventListener('click', backToMain)
-validateEmail();
+  };
 
-})
+  const backToMain = function () {
+    if (dissMissCTA) {
+      newsletterSuccesPopup.style.display = "none";
+      newsletter.style.display = "grid";
+    }
+  };
+  dissMissCTA.addEventListener("click", backToMain);
+  validateEmail();
+});
