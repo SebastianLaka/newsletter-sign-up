18db1e71e1b390c45c3689c2de25108decb90902
Sebastian <sebastianlakla@gmail.com>
2023-10-26 21:03:32 +0200

    Change classes structure in css and work with classList

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 "use strict";
 
+const newsletter = document.querySelector(".newsletter-body");
 const input = document.querySelector(".verification-area__input");
 const ctaButton = document.querySelector(".verification-area__cta");
 const emailInContent = document.querySelector(".newsletter-about__info--email");
+
 const newsletterSuccesPopup = document.querySelector(".newsletter-succes");
-const newsletter = document.querySelector(".newsletter-body");
 const errorInfo = document.querySelector(".label-area__requierd");
 const dissMissCTA = document.querySelector(".newsletter-succes-body__cta");
 const emailForValidation = document.getElementById("email");
 
-ctaButton.addEventListener("click", e => {
-  e.preventDefault();
+ctaButton.addEventListener("click", () => {
+ 
+  // const isValid = function (email) {
+  //   const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
+  //    regex.test(email)
+  // };
+  // isValid(email);
   const validateEmail = function () {
-    const isValid = function (email) {
-        const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
-         regex.test(email)
-      };
-      isValid(email);
-
     if (input.value ===  'email@company.com') {
-      newsletterSuccesPopup.style.display = "flex";
-      errorInfo.style.visibility = "hidden";
-      newsletter.style.display = "none";
-      emailInContent.innetText = emailForValidation;
-    } else if (input.value === "") {
+      newsletterSuccesPopup.classList.add('show-newsletter-succes')
+      errorInfo.classList.remove('show-error')
+    }else{
       errorInfo.classList.add('show-error')
     }
   };
   validateEmail();
 
   const backToMain = function () {
-    if (dissMissCTA) {
-      newsletterSuccesPopup.style.display = "none";
-      newsletter.style.display = "grid";
-    }
+    // if (dissMissCTA) {
+    //   newsletterSuccesPopup.style.display = "none";
+    //   newsletter.style.display = "grid";
+    // }
+    newsletter.classList.add('newsletter-show')
   };
   dissMissCTA.addEventListener("click", backToMain);
+  
 });
