b691a8714750360618e5f2e01e54d5ac8af464a6
Sebastian <sebastianlakla@gmail.com>
2023-11-01 18:09:40 +0100

    add validate for input

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 "use strict";
 
 const newsletter = document.querySelector(".newsletter-body");
-console.log(newsletter);
+
 const input = document.querySelector(".verification-area__input");
 const ctaButton = document.querySelector(".verification-area__cta");
 const emailInContent = document.querySelector(".newsletter-about__info--email");
 const newsletterSuccesPopup = document.querySelector(".newsletter-succes");
 const errorInfo = document.querySelector(".label-area__requierd");
-console.log(newsletterSuccesPopup);
+
 const dissMissCTA = document.querySelector(".newsletter-succes-body__cta");
 
-ctaButton.addEventListener("click", () => {
- 
-  // const isValid = function (email) {
-  //   const regex = /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
-  //    regex.test(email)
-  // };
-  // isValid(email);
-  const showError = function(){
-    errorInfo.classList.add('show-error')
-  }
-  const showPopup = function(){
-    newsletterSuccesPopup.classList.add('show-newsletter-succes')
-    newsletter.classList.add('hidden')
-    errorInfo.classList.remove('show-error')
-  }
-  const validateEmail = function() {
-    if (input.value === 'email@company.com') {
+ctaButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const showError = function () {
+    errorInfo.classList.add("show-error");
+  };
+  const showPopup = function () {
+    newsletterSuccesPopup.classList.add("show-newsletter-succes");
+    newsletter.classList.add("hidden");
+    errorInfo.classList.remove("show-error");
+  };
+  const validateEmail = function () {
+    const regex =
+      /(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@[*[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+]*/;
+    if (regex.test(input.value)) {
       showPopup();
-    }else{
+    } else {
       showError();
     }
   };
   validateEmail();
-  const backToMain = function() {
-    newsletter.classList.remove('hidden')
-    newsletterSuccesPopup.classList.remove('show-newsletter-succes')
+  const backToMain = function () {
+    newsletter.classList.remove("hidden");
+    newsletterSuccesPopup.classList.remove("show-newsletter-succes");
   };
   dissMissCTA.addEventListener("click", backToMain);
 });
